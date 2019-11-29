a42a4c37916f6ae5a7144dd27eb21234d5974956
Jorge Gonçalves <u.jorgea@gmail.com>
2019-11-29 12:05:03 +0000

    Initial commit

---
+++ app/src/main/java/pt/ulp/se201920_g01_20091502_21704830/MainActivity.kt
@@ -0,0 +1,12 @@
+package pt.ulp.se201920_g01_20091502_21704830
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
