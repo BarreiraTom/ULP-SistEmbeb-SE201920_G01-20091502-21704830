4c8214ba003f87ccd5571652afe7e1a28584681c
Jorge Gonçalves <u.jorgea@gmail.com>
2019-12-06 16:50:53 +0000

    side drawer menu added

--- app/src/main/java/pt/ulp/se201920_g01_20091502_21704830/MainActivity.kt
+++ app/src/main/java/pt/ulp/se201920_g01_20091502_21704830/MainActivity.kt
@@ -3,23 +3,84 @@ package pt.ulp.se201920_g01_20091502_21704830
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.Gravity
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
 import android.widget.EditText
+import android.widget.Toast
+import android.widget.Toolbar
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
+import kotlinx.android.synthetic.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+
+    lateinit var toolbar: androidx.appcompat.widget.Toolbar
+    lateinit var drawerLayout: DrawerLayout
+    lateinit var navView: NavigationView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.testlayout)
+        setContentView(R.layout.activity_main)
+
+
+        toolbar = findViewById(R.id.id_toolbar)
+        setSupportActionBar(toolbar)
+
+        drawerLayout = findViewById(R.id.id_drawer)
+        navView = findViewById(R.id.id_nav_viewer)
+
+
+        var toggle = ActionBarDrawerToggle(this, drawerLayout, toolbar, 0, 0)
+        supportActionBar!!.setDisplayHomeAsUpEnabled(false)
+        supportActionBar!!.setDisplayShowHomeEnabled(false)
+        navView.setNavigationItemSelectedListener(
+            object : NavigationView.OnNavigationItemSelectedListener {
+                override fun onNavigationItemSelected(item: MenuItem): Boolean {
+                    when (item.itemId) {
+                        R.id.login -> {
+                            Toast.makeText(this@MainActivity, "Login clicked", Toast.LENGTH_SHORT).show()
+                        }
+                        R.id.home -> {
+                            Toast.makeText(this@MainActivity, "Home clicked", Toast.LENGTH_SHORT).show()
+                        }
+                        R.id.manutencao -> {
+                            Toast.makeText(this@MainActivity, "Manutenção clicked", Toast.LENGTH_SHORT).show()
+                        }
+
+                    }
+                    return true
+                }
+
+            }
+        )
     }
 
-    fun testInsert(view: View?){
-        var fstName= findViewById<EditText>(R.id.fstNameEdit).text
-        var scndName= findViewById<EditText>(R.id.scndNameEdit).text
-        var Age= findViewById<EditText>(R.id.iddEdit).text
-
-        Log.println(Log.ASSERT, "FstName", "$fstName")
-        Log.println(Log.ASSERT, "ScndName", "$scndName")
-        Log.println(Log.ASSERT, "Age", "$Age")
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.right_side_menu, menu)
+        return true
     }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        if (item.itemId == R.id.right_menu) {
+            if (drawerLayout.isDrawerOpen(Gravity.RIGHT)) {
+                drawerLayout.closeDrawer(Gravity.RIGHT)
+            } else {
+                drawerLayout.openDrawer(Gravity.RIGHT)
+            }
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        drawerLayout.closeDrawer(GravityCompat.START)
+        return true
+    }
+
+
 }
