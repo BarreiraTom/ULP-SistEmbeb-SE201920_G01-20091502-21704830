00f5b5bebe1795786fd70d9bfa7f5d4588c5673f
Jorge Gonçalves <u.jorgea@gmail.com>
2019-12-07 16:15:12 +0000

    Implemented fragments

--- app/src/main/java/pt/ulp/se201920_g01_20091502_21704830/MainActivity.kt
+++ app/src/main/java/pt/ulp/se201920_g01_20091502_21704830/MainActivity.kt
@@ -1,31 +1,48 @@
 package pt.ulp.se201920_g01_20091502_21704830
 
+import android.net.Uri
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.view.Gravity
-import android.view.Menu
-import android.view.MenuItem
-import android.view.View
+import android.view.*
 import android.widget.EditText
 import android.widget.Toast
 import android.widget.Toolbar
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.fragment.app.FragmentTransaction
 import com.google.android.material.navigation.NavigationView
 import kotlinx.android.synthetic.*
+import pt.ulp.se201920_g01_20091502_21704830.Fragment.HomeFragment
+import pt.ulp.se201920_g01_20091502_21704830.Fragment.LoginFragment
+import pt.ulp.se201920_g01_20091502_21704830.Fragment.ManutencaoFragment
 
-class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener,
+    LoginFragment.OnFragmentInteractionListener, HomeFragment.OnFragmentInteractionListener,
+    ManutencaoFragment.OnFragmentInteractionListener {
 
     lateinit var toolbar: androidx.appcompat.widget.Toolbar
     lateinit var drawerLayout: DrawerLayout
     lateinit var navView: NavigationView
 
+    lateinit var loginFragment: LoginFragment
+    lateinit var homeFragment: HomeFragment
+    lateinit var manutencaoFragment: ManutencaoFragment
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        //Hide status bar
+
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                        WindowManager.LayoutParams.FLAG_FULLSCREEN)
+
+        ////////////////////////////////////////////////////
 
         toolbar = findViewById(R.id.id_toolbar)
         setSupportActionBar(toolbar)
@@ -34,29 +51,54 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         navView = findViewById(R.id.id_nav_viewer)
 
 
-        var toggle = ActionBarDrawerToggle(this, drawerLayout, toolbar, 0, 0)
+        //var toggle = ActionBarDrawerToggle(this, drawerLayout, toolbar, 0, 0)
         supportActionBar!!.setDisplayHomeAsUpEnabled(false)
         supportActionBar!!.setDisplayShowHomeEnabled(false)
-        navView.setNavigationItemSelectedListener(
-            object : NavigationView.OnNavigationItemSelectedListener {
-                override fun onNavigationItemSelected(item: MenuItem): Boolean {
-                    when (item.itemId) {
-                        R.id.login -> {
-                            Toast.makeText(this@MainActivity, "Login clicked", Toast.LENGTH_SHORT).show()
-                        }
-                        R.id.home -> {
-                            Toast.makeText(this@MainActivity, "Home clicked", Toast.LENGTH_SHORT).show()
-                        }
-                        R.id.manutencao -> {
-                            Toast.makeText(this@MainActivity, "Manutenção clicked", Toast.LENGTH_SHORT).show()
-                        }
 
+        loginFragment = LoginFragment.newInstance()
+        homeFragment = HomeFragment.newInstance("Parametro 1", "Parametro 2")
+        manutencaoFragment = ManutencaoFragment.newInstance("Podemos passar valores por aqui", "")
+
+        supportFragmentManager.beginTransaction().replace(R.id.id_fragment_container, loginFragment).commit()
+
+        navView.setNavigationItemSelectedListener(object :
+                                                      NavigationView.OnNavigationItemSelectedListener {
+            override fun onNavigationItemSelected(item: MenuItem): Boolean {
+
+
+                when (item.itemId) {
+                    R.id.login -> {
+                        supportFragmentManager.beginTransaction()
+                            .replace(R.id.id_fragment_container, loginFragment)
+                            .addToBackStack(loginFragment.toString())
+                            .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN).commit()
+                        Toast.makeText(this@MainActivity, "Login clicked", Toast.LENGTH_SHORT)
+                            .show()
                     }
-                    return true
+                    R.id.manutencao -> {
+                        supportFragmentManager.beginTransaction()
+                            .replace(R.id.id_fragment_container, manutencaoFragment)
+                            .addToBackStack(manutencaoFragment.toString())
+                            .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN).commit()
+                        Toast.makeText(this@MainActivity, "Login clicked", Toast.LENGTH_SHORT)
+                            .show()
+                        Toast.makeText(this@MainActivity, "Manutenção clicked", Toast.LENGTH_SHORT)
+                            .show()
+                    }
+                    R.id.home -> {
+                        supportFragmentManager.beginTransaction()
+                            .replace(R.id.id_fragment_container, homeFragment)
+                            .addToBackStack(homeFragment.toString())
+                            .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN).commit()
+                        Toast.makeText(this@MainActivity, "Login clicked", Toast.LENGTH_SHORT)
+                            .show()
+                        Toast.makeText(this@MainActivity, "Home clicked", Toast.LENGTH_SHORT).show()
+                    }
                 }
-
+                return true
             }
-        )
+
+        })
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -82,5 +124,8 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         return true
     }
 
+    override fun onFragmentInteraction(uri: Uri) {
+        Log.d("Implemented Interface", "Fragment changed")
+    }
 
 }
