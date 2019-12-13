7c9ca8590f1eca67dcbd8e19693349c844a92941
BarreiraTom <tomas.barreira14@outlook.pt>
2019-12-13 12:49:19 +0000

    Added Test Database - Frota

--- app/src/main/java/pt/ulp/se201920_g01_20091502_21704830/DatabaseHelper.kt
+++ app/src/main/java/pt/ulp/se201920_g01_20091502_21704830/DatabaseHelper.kt
@@ -5,66 +5,139 @@ import android.content.Context
 import android.database.Cursor
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
+import android.util.Log
 
 class DatabaseHelper(context: Context, factory: SQLiteDatabase.CursorFactory?) :
     SQLiteOpenHelper(context, DATABASE_NAME, factory, DATABASE_VERSION) {
 
-
+    //
+    //Not TODO: CREATION OF TABLES
+    //
     override fun onCreate(db: SQLiteDatabase) {
-        db.execSQL(
-            "CREATE TABLE IF NOT EXISTS $TABLE_NAME " +
-                    "($COLUMN_ID INTEGER PRIMARY KEY, $COLUMN_NAME TEXT, $COLUMN_AGE TEXT, $COLUMN_EMAIL TEXT)"
+        db.execSQL( "CREATE TABLE Utilizador ( " +
+                "ID INTEGER PRIMARY KEY AUTO_INCREMENT, " +
+                "USERNAME TEXT UNIQUE, " +
+                "NOME TEXT, " +
+                "EMAIL TEXT, " +
+                "PASSCODE TEXT " +
+                ")"
+//            "CREATE TABLE $TABLE_NAME " +
+//                    "($COLUMN_ID INTEGER PRIMARY KEY, $COLUMN_NAME TEXT, $COLUMN_AGE TEXT, $COLUMN_EMAIL TEXT)"
+        )
+        db.execSQL("CREATE TABLE Veiculo ( " +
+                "ID INTEGER PRIMARY KEY AUTO_INCREMENT, " +
+                "MATRICULA TEXT UNIQUE, " +
+                "MARCA TEXT, " +
+                "MODELO TEXT, " +
+                "ANO INT, " +
+                "FOREIGN KEY (ID) REFERENCES Utilizador(ID) " +
+                "ON UPDATE RESTRICT " +
+                "ON DELETE RESTRICT " +
+                ")"
+        )
+        db.execSQL("CREATE TABLE Seguro ( " +
+                "ID INTEGER PRIMARY KEY AUTO_INCREMENT, " +
+                "NOME TEXT, " +
+                "DATA_INI DATE, " +
+                "DATA_FIM DATE, " +
+                "VEICULO_ID INTEGER, " +
+                "FOREIGN KEY (VEICULO_ID) REFERENCES Veiculo(ID) " +
+                "ON UPDATE RESTRICT " +
+                "ON DELETE RESTRICT " +
+                ")"
+        )
+        db.execSQL("CREATE TABLE Manut_Prog ( " +
+                "ID INTEGER PRIMARY KEY AUTO_INCREMENT, " +
+                "DATA_D DATE, " +
+                "DESCRICAO TEXT, " +
+                "VEICULO_ID INTEGER, " +
+                "FOREIGN KEY (VEICULO_ID) REFERENCES Veiculo(ID) " +
+                "ON UPDATE RESTRICT " +
+                "ON DELETE RESTRICT " +
+                ")"
+        )
+        db.execSQL("CREATE TABLE Viagem_Realizada ( " +
+                "ID INTEGER PRIMARY KEY AUTO_INCREMENT, " +
+                "LOCAL_INI TEXT, " +
+                "LOCAL_DEST TEXT, " +
+                "DATA_D DATE, " +
+                "VEICULO_ID INTEGER, " +
+                "FOREIGN KEY (VEICULO_ID) REFERENCES Veiculo(ID) " +
+                "ON UPDATE RESTRICT " +
+                "ON DELETE RESTRICT " +
+                ")"
         )
     }
-//TODO
+
     override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
+        db.execSQL("DROP TABLE IF EXISTS Utilizador")
+        db.execSQL("DROP TABLE IF EXISTS Veiculo")
+        db.execSQL("DROP TABLE IF EXISTS Seguro")
+        db.execSQL("DROP TABLE IF EXISTS Manut_Prog")
+        db.execSQL("DROP TABLE IF EXISTS Viagem_Realizada")
 //        db.execSQL("DROP TABLE IF EXISTS $TABLE_NAME")
-//        onCreate(db)
+        onCreate(db)
     }
 
-    fun insertRow(name: String, age:String, email: String) {
-        val values = ContentValues()
-        values.put(COLUMN_NAME, name)
-        values.put(COLUMN_AGE, age)
-        values.put(COLUMN_EMAIL, email)
+    //
+    //Not TODO: QUERIES
+    //
 
-        val db = this.writableDatabase
-        db.insert(TABLE_NAME, null, values)
-        db.close()
-    }
+    fun getGeneralInfo(vehicle_ID: Int): Cursor?{
 
-    fun updateRow(row_id: String, name: String, age:String, email: String) {
-        val values = ContentValues()
-        values.put(COLUMN_NAME, name)
-        values.put(COLUMN_AGE, age)
-        values.put(COLUMN_EMAIL, email)
-
-        val db = this.writableDatabase
-        db.update(TABLE_NAME, values, "$COLUMN_ID = ?", arrayOf(row_id))
-        db.close()
-    }
-
-    fun deleteRow(row_id: String) {
-        val db = this.writableDatabase
-        db.delete(TABLE_NAME, "$COLUMN_ID = ?", arrayOf(row_id))
-        db.close()
-    }
-
-    fun getAllRow(): Cursor? {
+        //Necessary VEIC: ANO, VEIC: MARCA - Modelo, SEGURO: NOME - DATA_VAL
         val db = this.readableDatabase
-        return db.rawQuery("SELECT * FROM $TABLE_NAME", null)
+        var query= db.rawQuery("SELECT veiculo.MATRICULA, veiculo.MARCA, veiculo.MODELO, veiculo.ANO from veiculo WHERE veiculo.ID=$vehicle_ID", null)
+        //query= db.rawQuery("SELECT seguro.NOME, seguro.DATA_FIM from seguro WHERE seguro.ID=(SELECT max(seguro.ID) from seguro WHERE seguro.VEICULO_ID=$vehicle_ID)",null)
+        return query
     }
+
+//    fun insertRow(name: String, age:String, email: String) {
+//        val values = ContentValues()
+//        values.put(COLUMN_NAME, name)
+//        values.put(COLUMN_AGE, age)
+//        values.put(COLUMN_EMAIL, email)
+//
+//        val db = this.writableDatabase
+//        db.insert(TABLE_NAME, null, values)
+//        db.close()
+//    }
+//
+//    fun updateRow(row_id: String, name: String, age:String, email: String) {
+//        val values = ContentValues()
+//        values.put(COLUMN_NAME, name)
+//        values.put(COLUMN_AGE, age)
+//        values.put(COLUMN_EMAIL, email)
+//
+//        val db = this.writableDatabase
+//        db.update(TABLE_NAME, values, "$COLUMN_ID = ?", arrayOf(row_id))
+//        db.close()
+//    }
+//
+//    fun deleteRow(row_id: String) {
+//        val db = this.writableDatabase
+//        db.delete(TABLE_NAME, "$COLUMN_ID = ?", arrayOf(row_id))
+//        db.close()
+//    }
+//
+//    fun getAllRow(): Cursor? {
+//        val db = this.readableDatabase
+//        return db.rawQuery("SELECT * FROM $TABLE_NAME", null)
+//    }
+//
+//    fun getLastRow(): Cursor? {
+//        val db = this.readableDatabase
+//        return db.rawQuery("SELECT * FROM $TABLE_NAME WHERE $COLUMN_ID=(SELECT max($COLUMN_ID) FROM $TABLE_NAME)", null)
+//    }
 
     companion object {
         const val DATABASE_VERSION = 1
-        const val DATABASE_NAME = "BD_frota.db"
+        const val DATABASE_NAME = "frota_BD.db"
 
-        const val TABLE_NAME = "users"
-            const val COLUMN_ID = "id"
-            const val COLUMN_NAME = "name"
-            const val COLUMN_AGE = "age"
-            const val COLUMN_EMAIL = "email"
+//        const val TABLE_NAME = "users"
+//        const val COLUMN_ID = "id"
+//        const val COLUMN_NAME = "name"
+//        const val COLUMN_AGE = "age"
+//        const val COLUMN_EMAIL = "email"
     }
-
-
 }
--- app/src/main/java/pt/ulp/se201920_g01_20091502_21704830/MainActivity.kt
+++ app/src/main/java/pt/ulp/se201920_g01_20091502_21704830/MainActivity.kt
@@ -5,21 +5,58 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    val dbHandler = DatabaseHelper(this, null)
+    var dataList = ArrayList<HashMap<String, String>>()
+
+    lateinit var nameEditText:EditText
+    lateinit var ageEditText:EditText
+    lateinit var emailEditText:EditText
+    lateinit var modifyId:String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.testlayout)
     }
 
-    fun testInsert(view: View?){
-        var fstName= findViewById<EditText>(R.id.fstNameEdit).text
-        var scndName= findViewById<EditText>(R.id.scndNameEdit).text
-        var Age= findViewById<EditText>(R.id.iddEdit).text
+//    fun testInsert(view: View?){
+//        var fstName= findViewById<EditText>(R.id.fstNameEdit).text
+//        var scndName= findViewById<EditText>(R.id.scndNameEdit).text
+//        var Age= findViewById<EditText>(R.id.iddEdit).text
+//
+//        Log.println(Log.ASSERT, "FstName", "$fstName")
+//        Log.println(Log.ASSERT, "ScndName", "$scndName")
+//        Log.println(Log.ASSERT, "Age", "$Age")
+//
+//        DBHandler.insertRow(fstName as String, Age as String, scndName as String)
+//    }
+//
+    fun testgetLast(view: View?){
+        //var cursor= dbHandler.getLastRow()
+        //cursor!!.moveToFirst()
 
-        Log.println(Log.ASSERT, "FstName", "$fstName")
-        Log.println(Log.ASSERT, "ScndName", "$scndName")
-        Log.println(Log.ASSERT, "Age", "$Age")
+        //Log.println(Log.ASSERT, "CURSORNAME", "${cursor.getString(cursor.getColumnIndex(DatabaseHelper.COLUMN_NAME))}" )
+        //Log.println(Log.ASSERT, "CURSOREMAIL", "${cursor.getString(cursor.getColumnIndex(DatabaseHelper.COLUMN_EMAIL))}" )
+        //Log.println(Log.ASSERT, "CURSORAGE", "${cursor.getString(cursor.getColumnIndex(DatabaseHelper.COLUMN_AGE))}" )
+//        var id= cursor?.getString(cursor.getColumnIndex(DatabaseHelper.COLUMN_ID))
+//        var name= cursor?.getString(cursor.getColumnIndex(DatabaseHelper.COLUMN_NAME))
+//        var email= cursor?.getString(cursor.getColumnIndex(DatabaseHelper.COLUMN_EMAIL))
+//        var age= cursor?.getString(cursor.getColumnIndex(DatabaseHelper.COLUMN_AGE))
+//
+//        Log.println(Log.ASSERT, "GetID", "$id")
+//        Log.println(Log.ASSERT, "GetFstName", "$name")
+//        Log.println(Log.ASSERT, "GetScndName", "$email")
+//        Log.println(Log.ASSERT, "GetAge", "$age")
+    }
+
+    fun add(v:View){
+        val name = findViewById<EditText>(R.id.fstNameEdit).text.toString()
+        val age = findViewById<EditText>(R.id.iddEdit).text.toString()
+        val email = findViewById<EditText>(R.id.scndNameEdit).text.toString()
+        //dbHandler.insertRow(name, age, email)
+        Toast.makeText(this, "Data Addeded", Toast.LENGTH_SHORT).show()
     }
 }
