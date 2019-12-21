9724f6d623218549400fd7b7b838c11d57be17c1
Jorge Gonçalves <u.jorgea@gmail.com>
2019-12-21 14:18:10 +0000

    pre activity insert

--- app/src/main/java/pt/ulp/se201920_g01_20091502_21704830/Fragment/ManutencaoFragment.kt
+++ app/src/main/java/pt/ulp/se201920_g01_20091502_21704830/Fragment/ManutencaoFragment.kt
@@ -1,13 +1,15 @@
 package pt.ulp.se201920_g01_20091502_21704830.Fragment
 
-import android.content.Context
+
 import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 
+import androidx.fragment.app.FragmentTransaction
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+import pt.ulp.se201920_g01_20091502_21704830.Fragment.*
 import pt.ulp.se201920_g01_20091502_21704830.R
 
 // TODO: Rename parameter arguments, choose names that match
@@ -23,11 +25,12 @@ private const val ARG_PARAM2 = "param2"
  * Use the [ManutencaoFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class ManutencaoFragment : Fragment() {
+class ManutencaoFragment : Fragment(), InserirManutencaoFragment.OnFragmentInteractionListener {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
-    private var listener: OnFragmentInteractionListener? = null
+
+    lateinit var inserirManutencaoFragment: InserirManutencaoFragment
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -39,62 +42,26 @@ class ManutencaoFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
+        val viewF: View = inflater.inflate(R.layout.fragment_manutencao, container, false)
+
+        inserirManutencaoFragment = InserirManutencaoFragment.newInstance("", "")
+
+        val fab: FloatingActionButton = viewF.findViewById(R.id.fab)
+        fab.setOnClickListener { view ->
+            activity!!.supportFragmentManager.beginTransaction().remove(ManutencaoFragment())
+                .replace(R.id.nav_host_fragment, inserirManutencaoFragment)
+                .addToBackStack(inserirManutencaoFragment.toString())
+                .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN).commit()
+//            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                .setAction("Action", null).show()
+        }
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_manutencao, container, false)
+        return viewF
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    fun onButtonPressed(uri: Uri) {
-        listener?.onFragmentInteraction(uri)
-    }
-
-    override fun onAttach(context: Context) {
-        super.onAttach(context)
-        if (context is OnFragmentInteractionListener) {
-            listener = context
-        } else {
-            throw RuntimeException(
-                context.toString() + " must implement OnFragmentInteractionListener")
-        }
-    }
-
-    override fun onDetach() {
-        super.onDetach()
-        listener = null
-    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     *
-     *
-     * See the Android Training lesson [Communicating with Other Fragments]
-     * (http://developer.android.com/training/basics/fragments/communicating.html)
-     * for more information.
-     */
-    interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        fun onFragmentInteraction(uri: Uri)
-    }
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ManutencaoFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) = ManutencaoFragment().apply {
-            arguments = Bundle().apply {
-                putString(ARG_PARAM1, param1)
-                putString(ARG_PARAM2, param2)
-            }
-        }
+    override fun onFragmentInteraction(uri: Uri) {
+        TODO(
+            "not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 }
--- app/src/main/java/pt/ulp/se201920_g01_20091502_21704830/MainActivity.kt
+++ app/src/main/java/pt/ulp/se201920_g01_20091502_21704830/MainActivity.kt
@@ -1,40 +1,32 @@
 package pt.ulp.se201920_g01_20091502_21704830
 
-import android.net.Uri
-import android.os.Build
+
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.view.*
-import android.widget.EditText
-import android.widget.Toast
-import android.widget.Toolbar
-import androidx.appcompat.app.ActionBarDrawerToggle
-import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
-import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentActivity
-import androidx.fragment.app.FragmentTransaction
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.navigation.NavigationView
-import kotlinx.android.synthetic.*
-import pt.ulp.se201920_g01_20091502_21704830.Fragment.*
 
-class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener,
-    LoginFragment.OnFragmentInteractionListener, HomeFragment.OnFragmentInteractionListener,
-    ManutencaoFragment.OnFragmentInteractionListener {
+
+class MainActivity : AppCompatActivity(){
 
     lateinit var toolbar: androidx.appcompat.widget.Toolbar
     lateinit var drawerLayout: DrawerLayout
     lateinit var navView: NavigationView
+    private lateinit var appBarConfiguration: AppBarConfiguration
 
-    lateinit var loginFragment: LoginFragment
-    lateinit var homeFragment: HomeFragment
-    lateinit var manutencaoFragment: ManutencaoFragment
-    lateinit var reusableFragment: ReusableFragment
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        toolbar = findViewById(R.id.id_toolbar)
+        setSupportActionBar(toolbar)
 
         //Hide status bar
 
@@ -42,9 +34,6 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
                         WindowManager.LayoutParams.FLAG_FULLSCREEN)
 
         ////////////////////////////////////////////////////
-
-        toolbar = findViewById(R.id.id_toolbar)
-        setSupportActionBar(toolbar)
 
         drawerLayout = findViewById(R.id.id_drawer)
         navView = findViewById(R.id.id_nav_viewer)
@@ -54,69 +43,12 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         supportActionBar!!.setDisplayHomeAsUpEnabled(false)
         supportActionBar!!.setDisplayShowHomeEnabled(false)
 
-        loginFragment = LoginFragment.newInstance()
-        homeFragment = HomeFragment.newInstance("Parametro 1", "Parametro 2")
-        manutencaoFragment = ManutencaoFragment.newInstance("Podemos passar valores por aqui", "")
-        reusableFragment = ReusableFragment.newInstance("", "")
+        val navController = findNavController(R.id.nav_host_fragment)
 
-        supportFragmentManager.beginTransaction().replace(R.id.id_fragment_container, loginFragment)
-            .commit()
+        appBarConfiguration = AppBarConfiguration(
+            setOf(R.id.nav_home, R.id.nav_manutencao), drawerLayout)
 
-        navView.setNavigationItemSelectedListener(object :
-                                                      NavigationView.OnNavigationItemSelectedListener {
-            override fun onNavigationItemSelected(item: MenuItem): Boolean {
-
-
-                when (item.itemId) {
-                    R.id.login -> {
-                        supportFragmentManager.beginTransaction()
-                            .replace(R.id.id_fragment_container, loginFragment)
-                            .addToBackStack(loginFragment.toString())
-                            .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN).commit()
-                        Toast.makeText(this@MainActivity, "Login clicked", Toast.LENGTH_SHORT)
-                            .show()
-                    }
-                    R.id.manutencao -> {
-                        supportFragmentManager.beginTransaction()
-                            .replace(R.id.id_fragment_container, manutencaoFragment)
-                            .addToBackStack(manutencaoFragment.toString())
-                            .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN).commit()
-                        Toast.makeText(this@MainActivity, "Manutenção clicked", Toast.LENGTH_SHORT)
-                            .show()
-                    }
-                    R.id.home -> {
-                        supportFragmentManager.beginTransaction()
-                            .replace(R.id.id_fragment_container, homeFragment)
-                            .addToBackStack(homeFragment.toString())
-                            .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN).commit()
-                        Toast.makeText(this@MainActivity, "Home clicked", Toast.LENGTH_SHORT).show()
-                    }
-                    R.id.viagens -> {
-                        supportFragmentManager.beginTransaction()
-                            .replace(R.id.id_fragment_container, reusableFragment)
-                            .addToBackStack(reusableFragment.toString())
-                            .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN).commit()
-                        Toast.makeText(this@MainActivity, "Home clicked", Toast.LENGTH_SHORT).show()
-                    }
-                    R.id.abastecimentos -> {
-                        supportFragmentManager.beginTransaction()
-                            .replace(R.id.id_fragment_container, reusableFragment)
-                            .addToBackStack(reusableFragment.toString())
-                            .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN).commit()
-                        Toast.makeText(this@MainActivity, "Home clicked", Toast.LENGTH_SHORT).show()
-                    }
-                    R.id.logout -> {
-                        supportFragmentManager.beginTransaction()
-                            .replace(R.id.id_fragment_container, reusableFragment)
-                            .addToBackStack(reusableFragment.toString())
-                            .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN).commit()
-                        Toast.makeText(this@MainActivity, "Home clicked", Toast.LENGTH_SHORT).show()
-                    }
-                }
-                return true
-            }
-
-        })
+        navView.setupWithNavController(navController)
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -137,13 +69,9 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         return super.onOptionsItemSelected(item)
     }
 
-    override fun onNavigationItemSelected(item: MenuItem): Boolean {
-        drawerLayout.closeDrawer(GravityCompat.START)
-        return true
-    }
-
-    override fun onFragmentInteraction(uri: Uri) {
-        Log.d("Implemented Interface", "Fragment changed")
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment)
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 
 }
