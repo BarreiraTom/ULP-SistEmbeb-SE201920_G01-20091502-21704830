db070549fc50b388a8bbc4507ffc232c29000ea8
Jorge Gonçalves <u.jorgea@gmail.com>
2020-01-02 19:03:23 +0000

    conflit resolve

--- app/src/main/java/pt/ulp/se201920_g01_20091502_21704830/MainActivity.kt
+++ app/src/main/java/pt/ulp/se201920_g01_20091502_21704830/MainActivity.kt
@@ -10,7 +10,41 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+<<<<<<< Updated upstream
         setContentView(R.layout.testlayout)
+=======
+        setContentView(R.layout.activity_main)
+
+        toolbar = findViewById(R.id.id_toolbar)
+        setSupportActionBar(toolbar)
+
+        //Hide status bar
+
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                        WindowManager.LayoutParams.FLAG_FULLSCREEN)
+
+        ////////////////////////////////////////////////////
+
+        drawerLayout = findViewById(R.id.id_drawer)
+        val navView = findViewById<NavigationView>(R.id.id_nav_viewer)
+
+
+
+        supportActionBar!!.setDisplayHomeAsUpEnabled(false)
+        supportActionBar!!.setDisplayShowHomeEnabled(false)
+
+        val navController = findNavController(R.id.nav_host_fragment)
+
+        appBarConfiguration = AppBarConfiguration(
+            setOf(R.id.nav_home, R.id.nav_manutencao), drawerLayout)
+
+        navView.setupWithNavController(navController)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.right_side_menu, menu)
+        return true
+>>>>>>> Stashed changes
     }
 
     fun testInsert(view: View?){
