98049cc7ce4787972daa5a1e958ea380199b5174
Jorge Gonçalves <u.jorgea@gmail.com>
2020-01-03 20:03:57 +0000

    added recycler view to abastecimentos

---
+++ app/src/main/java/pt/ulp/se201920_g01_20091502_21704830/Dataclasses/dataclass_abast.kt
@@ -0,0 +1,3 @@
+package pt.ulp.se201920_g01_20091502_21704830.Dataclasses
+
+data class dataclass_abast (val data: String, val local: String, val quantidade: String, val valor_gasto: String)
--- app/src/main/java/pt/ulp/se201920_g01_20091502_21704830/Fragment/AbastecimentosFragment.kt
+++ app/src/main/java/pt/ulp/se201920_g01_20091502_21704830/Fragment/AbastecimentosFragment.kt
@@ -6,7 +6,11 @@ import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.*
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import kotlinx.android.synthetic.main.fragment_abastecimentos.*
+import pt.ulp.se201920_g01_20091502_21704830.Adapters.adapter_abast
+import pt.ulp.se201920_g01_20091502_21704830.Dataclasses.dataclass_abast
 import pt.ulp.se201920_g01_20091502_21704830.InserirAbastActivity
 import pt.ulp.se201920_g01_20091502_21704830.R
 
@@ -25,9 +29,17 @@ class AbastecimentosFragment : Fragment() {
         fab.setOnClickListener { view ->
             val intent = Intent(context, InserirAbastActivity::class.java)
             startActivity(intent)
-            }
+        }
         return viewF
     }
 
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        val abastecimentos = listOf(dataclass_abast("03-01-2020", "Galp", "30L", "45€"),
+                                    dataclass_abast("02-01-2020", "BP", "20L", "30€"))
 
+        recycler_view_abast.layoutManager = LinearLayoutManager(activity)
+        recycler_view_abast.adapter = adapter_abast(abastecimentos)
+
+        super.onActivityCreated(savedInstanceState)
+    }
 }
